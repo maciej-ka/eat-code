37478cdeb56d8ab634dffd6183fa4a19bb65bdb8
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-05-13 00:02:26 +0200

    feat: few basic operations on pointers and arrays

--- 3149/1.c
+++ 3149/1.c
@@ -1,5 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
- * Note: The returned array must be malloced, assume caller calls free().
+ * My first ever malloc usage.
+ * The returned array must be malloced, assume caller calls free().
  */
 int* findPermutation(int* nums, int numsSize, int* returnSize) {
+  int* res = malloc(numsSize * sizeof(int));
+  *returnSize = numsSize;
+  res[0] = 5;
+  res[1] = 6;
+  res[2] = 7;
+  return res;
+}
+
+/**
+ * Test usage.
+ */
+int main() {
+  int nums[] = {1,0,2};
+  int* returnSize = malloc(sizeof(int));
+  int* res = findPermutation(nums, 3, returnSize);
+
+  for (int i=0;i<*returnSize;i++) {
+    printf("%d", res[i]);
+  }
+  puts("");
 }
