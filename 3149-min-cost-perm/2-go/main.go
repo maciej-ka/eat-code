95b145e4cfe9000d6eccabe463dd074c94892468
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-06-02 15:24:07 +0200

    feat: calc scores

--- 3149-min-cost-perm/2-go/main.go
+++ 3149-min-cost-perm/2-go/main.go
@@ -4,10 +4,10 @@ import (
   "fmt"
 )
 
-type step struct {
+type Step struct {
+  pos int
   perm []int
   appl []int
-  pos int
   scores []int
 }
 
@@ -39,16 +39,23 @@ func Calc(a, b []int) int {
   return best
 }
 
-// func CalcScores(step s) {
-//
-// }
+func CalcScores(s Step) {
+  var a, b []int
+  bi := 1
+  for i := range s.scores {
+    if i <= s.pos { a = s.perm[i:i+1] } else { a = s.perm[s.pos+1:] }
+    if bi <= s.pos { b = s.appl[bi:bi+1]} else { b = s.appl[s.pos+1:] }
+    s.scores[i] = Calc(a, b)
+    bi = (bi + 1) % len(s.scores)
+  }
+}
 
 func findPermutation(nums []int) []int {
   // create zero step
-  root := step{
+  root := Step {
+    pos: 0,
     perm: Seq(len(nums)),
     appl: Pull(nums[0], 0, Seq(len(nums))),
-    pos: 0,
     scores: make([]int, len(nums)),
   }
   fmt.Println(root)
@@ -59,7 +66,7 @@ func main() {
   findPermutation([]int{3,1,0,2})
 }
 
-// type step struct {
+// type Step struct {
 //   // scores
 //   // total
 //   // children
