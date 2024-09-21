b55927d8c34f887d7124eadf1c075c3e530755ad
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-09-21 18:32:41 +0200

    feat: faster ruby solution without arrays

---
+++ 213-robber/2-rb/solution.rb
@@ -0,0 +1,31 @@
+# not using arrays
+# in hope of faster result
+def rob(nums)
+  return 0 if !nums
+  return nums.max if nums.length <= 3
+
+  temp = 0
+  solution_prev = 0
+  solution = nums[0]
+
+  i = 1
+  while i < nums.length - 1
+    temp = solution
+    solution = [solution, nums[i] + solution_prev].max
+    solution_prev = temp
+    i += 1
+  end
+
+  skipped_prev = 0
+  skipped = nums[1]
+
+  i = 2
+  while i < nums.length - 2
+    temp = skipped
+    skipped = [skipped, nums[i] + skipped_prev].max
+    skipped_prev = temp
+    i += 1
+  end
+
+  [solution, nums[-1] + skipped].max
+end
