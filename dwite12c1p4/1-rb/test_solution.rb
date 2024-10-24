ebd718b311cf1e30b1643441816ca396f51beb6c
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-10-24 22:01:00 +0200

    test: more cases

--- dwite12c1p4/1-rb/test_solution.rb
+++ dwite12c1p4/1-rb/test_solution.rb
@@ -17,4 +17,16 @@ class Test < Minitest::Test
   def test_4
     assert_equal "15 6", solve("(((((1 1) 1) 1) 1) 1)")
   end
+
+  def test_5
+    assert_equal "0 3", solve("3")
+  end
+
+  def test_6
+    assert_equal "30 13", solve("((1 2) (((10 10) (3 4)) ((((1 1) 1) 1) 1)))")
+  end
+
+  def test_7
+    assert_equal "25 40", solve("(((1 2) (3 4)) ((6 7) (8 9)))")
+  end
 end
