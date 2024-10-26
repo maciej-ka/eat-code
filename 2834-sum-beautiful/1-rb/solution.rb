d3bacd85cf0237652a74196d2243390b1edb6523
maciej-ka <maciej.kasprzyk.it@gmail.com>
2024-10-26 20:55:11 +0200

    refactor: describe code sections with inline comments

--- 2834-sum-beautiful/1-rb/solution.rb
+++ 2834-sum-beautiful/1-rb/solution.rb
@@ -2,16 +2,18 @@
 # @param {Integer} target
 # @return {Integer}
 def minimum_possible_sum(n, target)
+  # one
   return 1 if n == 1
 
+  # fit small triangle
   half_target = (target / 2).floor
   modulo_cut = (10 ** 9) + 7
   if (n <= half_target)
     return ((n * (n + 1)) / 2) % modulo_cut
   end
 
+  # some element are over small triangle
   rest = n - half_target
-
   low = (half_target * (half_target + 1)) / 2
   podest = rest * target
   high = (rest * (rest - 1)) / 2
