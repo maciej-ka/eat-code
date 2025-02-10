c86c3d4ededf788948a4d984980646887f48bfe5
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-02-10 15:02:31 +0100

    feat: top k frequent, php solution

--- 347-top-k-frequent/2-php/src/Solution.php
+++ 347-top-k-frequent/2-php/src/Solution.php
@@ -3,7 +3,18 @@
 namespace App;
 
 class Solution {
-  public function sayHello(): string {
-    return "Hello, World!";
-  }
+    /**
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer[]
+     */
+    function topKFrequent($nums, $k): array {
+      $counts = [];
+      foreach ($nums as $v) {
+        $counts[$v] = array_key_exists($v, $counts) ? $counts[$v] + 1 : 1;
+      }
+      arsort($counts);
+      return array_slice(array_keys($counts), 0, $k);
+    }
 }
+
--- 347-top-k-frequent/2-php/tests/SolutionTest.php
+++ 347-top-k-frequent/2-php/tests/SolutionTest.php
@@ -4,8 +4,21 @@ use PHPUnit\Framework\TestCase;
 use App\Solution;
 
 class SolutionTest extends TestCase {
-    public function testSayHello() {
+    public function test(): void {
         $hello = new Solution();
-        $this->assertEquals("Hello, World!", $hello->sayHello());
+        $actual = $hello->topKFrequent([1,1,1,2,2,3], 2);
+        $this->assertEquals([1, 2], $actual);
+    }
+
+    public function testNegative(): void {
+        $hello = new Solution();
+        $actual = $hello->topKFrequent([-1,-1], 1);
+        $this->assertEquals([-1], $actual);
+    }
+
+    public function testCase15(): void {
+        $hello = new Solution();
+        $actual = $hello->topKFrequent([5,3,1,1,1,3,73,1], 1);
+        $this->assertEquals([1], $actual);
     }
 }
