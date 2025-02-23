9a10ada680aa378ef16acb11d5ab43010c39029f
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-02-23 12:49:48 +0100

    feat: 3399 elixir solution finished

--- 3399-shortest-identical-slice/5-ex/lib/solution.ex
+++ 3399-shortest-identical-slice/5-ex/lib/solution.ex
@@ -4,7 +4,11 @@ defmodule Solution do
     cond do
       solution_one(s, num_ops, ?0) -> 1
       solution_one(s, num_ops, ?1) -> 1
-      :true -> solution_n(s, num_ops)
+      :true ->
+        counts = count_lengths(s)
+        lengths = Enum.sort(Map.keys(counts), &>=/2)
+        score_possible? = fn score -> score_possible?(lengths, counts, num_ops, score) end
+        find_solution(score_possible?, 2, hd(lengths))
     end
   end
 
@@ -16,17 +20,35 @@ defmodule Solution do
   def flip(?0), do: ?1
   def flip(?1), do: ?0
 
-  def solution_n(_, _), do: 2
-
-  def lengths(<<a, rest::binary>>), do: lengths(rest, a, 1, %{})
-  def lengths(<<a, rest::binary>>, a, len, acc), do: lengths(rest, a, len + 1, acc)
-  def lengths(<<a, rest::binary>>, _, len, acc) do
-    count = Map.get(acc, len, 0)
-    acc = Map.put(acc, len, count + 1)
-    lengths(rest, a, 1, acc)
+  def count_lengths(<<a, rest::binary>>), do: count_lengths(rest, a, 1, %{})
+  def count_lengths(<<a, rest::binary>>, a, length, acc), do: count_lengths(rest, a, length + 1, acc)
+  def count_lengths(<<a, rest::binary>>, _, length, acc) do
+    count = Map.get(acc, length, 0)
+    acc = Map.put(acc, length, count + 1)
+    count_lengths(rest, a, 1, acc)
   end
-  def lengths("", _, len, acc) do
-    count = Map.get(acc, len, 0)
-    Map.put(acc, len, count + 1)
+  def count_lengths("", _, length, acc) do
+    count = Map.get(acc, length, 0)
+    Map.put(acc, length, count + 1)
+  end
+
+  def score_possible?(lengths, counts, num_ops, score), do: score_possible?(lengths, counts, num_ops, score, 0)
+  def score_possible?(_, _, num_ops, _, acc) when acc > num_ops, do: :false
+  def score_possible?([], _, _, _, _), do: :true
+  def score_possible?([length | rest], counts, num_ops, score, acc) do
+    inc = div(length, score + 1) * counts[length]
+    score_possible?(rest, counts, num_ops, score, acc + inc)
+  end
+
+  # def find_solution(score_possible?, lo, hi)
+  def find_solution(_, lo, hi) when lo >= hi, do: lo
+  def find_solution(score_possible?, lo, hi) do
+    m = div(lo + hi, 2)
+    if score_possible?.(m) do
+      find_solution(score_possible?, lo, m)
+    else
+      find_solution(score_possible?, m + 1, hi)
+    end
   end
 end
+
--- 3399-shortest-identical-slice/5-ex/test/solution_test.exs
+++ 3399-shortest-identical-slice/5-ex/test/solution_test.exs
@@ -30,22 +30,51 @@ defmodule SolutionTest do
   end
 
   test "case 8" do
-    assert Solution.lengths("1") == %{1 => 1}
+    assert Solution.count_lengths("1") == %{1 => 1}
   end
 
   test "case 9" do
-    assert Solution.lengths("100") == %{1 => 1, 2 => 1}
+    assert Solution.count_lengths("100") == %{1 => 1, 2 => 1}
   end
 
   test "case 10" do
-    assert Solution.lengths("000001") == %{1 => 1, 5 => 1}
+    assert Solution.count_lengths("000001") == %{1 => 1, 5 => 1}
   end
 
   test "case 11" do
-    assert Solution.lengths("0000") == %{4 => 1}
+    assert Solution.count_lengths("0000") == %{4 => 1}
   end
 
   test "case 12" do
-    assert Solution.lengths("0101") == %{1 => 4}
+    assert Solution.count_lengths("0101") == %{1 => 4}
   end
+
+  test "case 13" do
+    assert Solution.score_possible?([5, 1], %{5 => 1, 1 => 1}, 1, 2) == :true
+  end
+
+  test "case 14" do
+    assert Solution.score_possible?([6, 1], %{6 => 1, 1 => 1}, 1, 2) == :false
+  end
+
+  test "case 15" do
+    assert Solution.score_possible?([6, 1], %{6 => 1, 1 => 1}, 1, 3) == :true
+  end
+
+  test "case 16" do
+    assert Solution.score_possible?([6, 1], %{6 => 1, 1 => 1}, 2, 2) == :true
+  end
+
+  test "case 17" do
+    assert Solution.min_length("000001", 1) == 2
+  end
+
+  test "case 18" do
+    assert Solution.min_length("0000", 2) == 1
+  end
+
+  test "case 19" do
+    assert Solution.min_length("0101", 0) == 1
+  end
+
 end
