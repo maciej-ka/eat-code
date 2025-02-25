265705fd93cc40385a69504666f9d483ad4d7875
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-02-25 15:00:48 +0100

    docs: update notes on 1524

--- 1524-odd-sum-subarrays/1-js/solution.js
+++ 1524-odd-sum-subarrays/1-js/solution.js
@@ -3,6 +3,16 @@
  * @return {number}
  */
 var numOfSubarrays = function(arr) {
+  // scan for at least one odd
+  if (arr.findIndex(x => x % 2) < 0) {
+    return 0
+  }
+
+  let result = 1
+  for (let i=(arr.length - 1); i>0; i--) {
+    result = (result << 1) % 1000000007
+  }
+  return result
 };
 
-export default numberOfSubarray;
+export default numOfSubarrays;
--- 1524-odd-sum-subarrays/1-js/solution.test.js
+++ 1524-odd-sum-subarrays/1-js/solution.test.js
@@ -2,16 +2,16 @@ import { expect, test, vitest } from 'vitest'
 import solution from "./solution";
 
 test('case 1', () => {
-  const actual = solution([1,3,5], 1);
+  const actual = solution([1,3,5]);
   expect(actual).toEqual(4);
 });
 
 test('case 2', () => {
-  const actual = solution([2,4,6], 2);
+  const actual = solution([2,4,6]);
   expect(actual).toEqual(0);
 });
 
 test('case 3', () => {
-  const actual = solution([1,2,3,4,5,6,7], 0);
+  const actual = solution([1,2,3,4,5,6,7]);
   expect(actual).toEqual(16);
 });
