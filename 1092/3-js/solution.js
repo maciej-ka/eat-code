71b4049e0e596cd44d7f21cf3ab090b8edbed4dd
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-02-28 17:28:11 +0100

    feat: remove debug comments from 1092

--- 1092/3-js/solution.js
+++ 1092/3-js/solution.js
@@ -23,19 +23,16 @@ var shortestCommonSupersequence = function (str1, str2) {
 
   for (let len1 = 0; len1 <= str1.length; len1++) {
     for (let len2 = 0; len2 <= str2.length; len2++) {
-      // console.log(len1, len2);
       let next = 0
 
       if (len1) {
         next = (LEN & solution[len1 - 1][len2]) + 4 + STR1;
-        // console.log('str1');
       }
 
       if (len2) {
         const candidate = (LEN & solution[len1][len2 - 1]) + 4 + STR2;
         if (!next || candidate < next) {
           next = candidate;
-          // console.log('str2');
         }
       }
 
@@ -43,11 +40,9 @@ var shortestCommonSupersequence = function (str1, str2) {
         const candidate = (LEN & solution[len1 - 1][len2 - 1]) + 4 + BOTH;
         if (!next || candidate < next) {
           next = candidate;
-          // console.log('both');
         }
       }
 
-      // console.log(next);
       solution[len1][len2] = next;
     }
   }
@@ -58,20 +53,16 @@ var shortestCommonSupersequence = function (str1, str2) {
   let result = ""
 
   while (i1 >= 0 && i2 >= 0) {
-    // console.log(i1, i2);
     switch(DIR & solution[i1][i2]) {
       case STR1:
-        // console.log("str1");
         i1--
         result = (str1[i1] || "") + result
         break
       case STR2:
-        // console.log("str2");
         i2--
         result = (str2[i2] || "") + result
         break
       default:
-        // console.log("both");
         i1--
         i2--
         result = (str1[i1] || "") + result
