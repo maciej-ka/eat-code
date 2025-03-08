07e84b7756ac6dc645bba92e0f0d705f5e0c7521
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-08 02:17:33 +0100

    feat: 2965 Go solution

--- 2965/2-go/solution.go
+++ 2965/2-go/solution.go
@@ -1,5 +1,17 @@
 package solution
 
 func FindMissingAndRepeatedValues(grid [][]int) []int {
-  return []int{2,4} 
+  counts := make([]int, len(grid) * len(grid) + 1)
+  for i := range grid {
+    for _, value := range grid[i] {
+      counts[value]++
+    }
+  }
+
+  var num1, num2 int
+  for value, count := range counts {
+    if (count == 2) { num1 = value }
+    if (count == 0) { num2 = value }
+  }
+  return []int{num1, num2}
 }
