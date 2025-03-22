436b08d307d233d98d51e4609cc38b03c74f84a4
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-22 13:01:51 +0100

    feat: 2685 solution remarks

--- 2685/1-js/solution.js
+++ 2685/1-js/solution.js
@@ -10,30 +10,30 @@ var countCompleteComponents = function(n, edges) {
   for (let i in p) { p[i] = i }
   function find(i) { return i === p[i] ? i : p[i] = find(p[i]) }
 
-  // Edge count for each graph
-  const eCount = new Uint8Array(n)
+  // edge count for each graph
+  const eCount = new Uint16Array(n)
 
   for (let edge of edges) {
     const p0 = find(edge[0])
     const p1 = find(edge[1])
-    eCount[p0] += 1
+    eCount[p0]++
 
-    // merge into first vertice
+    // merge into first graph
     if (p0 !== p1) {
       p[edge[1]] = p[p1] = p0
       eCount[p0] += eCount[p1]
     }
   }
 
-  // Vertice count for each graph
+  // vertice count for each graph
   const vCount = new Map()
   for (let i in p) {
     const id = find(i)
     vCount.set(id, (vCount.get(id) || 0) + 1)
   }
 
-  // Graph with n vertices is complete
-  // when it has 1 + 2 + n-1 distinct edges
+  // graph with n vertices is complete
+  // when it has 1 + 2 + (n - 1) distinct edges
   let result = 0
   for (let id of vCount.keys()) {
     const v = vCount.get(id)
