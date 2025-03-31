947725a4d7c78462e328a706ead6bbe0e3187ece
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-03-31 12:35:16 +0200

    feat: 2551 solution

--- 2551/1-js/solution.js
+++ 2551/1-js/solution.js
@@ -10,6 +10,41 @@
 
 // bag cannot have two subarrays
 
+// task is about creating k - 1 divisions
+// every division decides on two values
+
+// first and last element
+// will always contribute to score
+
+// score is sum of bags
+// bags are sum of edges
+// so score is sum of edges
+
 var putMarbles = function(weights, k) {
-    
+  // sum for each possible division point
+  // divs[0] is division between 0 and 1
+  const divs = new Uint32Array(weights.length - 1)
+  for (let i = 0; i < weights.length - 1; i++) {
+    divs[i] = weights[i] + weights[i + 1]
+  }
+
+  // sort descending
+  divs.sort((a, b) => b - a)
+  console.log(divs);
+
+  // to have k bags, we need to divide k - 1
+  // result is difference between k -1 lowest and highest
+  let result = 0
+  for (let i = 0; i < k - 1; i++) {
+    result += divs[i] - divs[divs.length - 1 - i]
+  }
+  return result
 };
+
+export default putMarbles
+
+// example
+// 1,3,5,1
+//
+// divs
+// 4,8,6
--- 2551/1-js/solution.test.js
+++ 2551/1-js/solution.test.js
@@ -12,7 +12,7 @@ import solution from "./solution";
 // result: 4
 test('case 1', () => {
   const actual = solution([1,3,5,1], 2);
-  expect(actual).toEqual();
+  expect(actual).toEqual(4);
 });
 
 // max and min same
@@ -22,5 +22,19 @@ test('case 1', () => {
 // result: 0
 test('case 2', () => {
   const actual = solution([1,3], 2);
-  expect(actual).toEqual();
+  expect(actual).toEqual(0);
+});
+
+// min: 4
+// k1 2: 1, 5, 1
+// k2 2: 1, 5, 1
+//
+// max: 8
+// k1 6: 1, 5
+// k2 2: 1, 1, 5, 1
+//
+// result: 4
+test('case 2', () => {
+  const actual = solution([1,5,1,1,5,1], 2);
+  expect(actual).toEqual(4);
 });
