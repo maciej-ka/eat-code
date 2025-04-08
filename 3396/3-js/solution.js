6930b0287e7f7bc90edc865756c3fa2c61d9ee8d
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-04-08 08:20:50 +0200

    refactor: 3396 remove debug console

--- 3396/3-js/solution.js
+++ 3396/3-js/solution.js
@@ -8,13 +8,11 @@ var minimumOperations = function(nums) {
   for (let i = nums.length - 1; i >= 0; i--) {
     const mask = 1n << BigInt(nums[i])
     if (lastSeen & mask) {
-      console.log(lastSeen.toString(2));
       return Math.ceil((i + 1) / 3)
     }
     lastSeen |= mask
   }
 
-  console.log(lastSeen.toString(2));
   return 0
 };
 
