b06194561a4b9889c19f575437563fbce5229d86
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-08-31 01:29:14 +0200

    changes

--- 0/0036/6-rkt/solution.rkt
+++ 0/0036/6-rkt/solution.rkt
@@ -1,13 +1,16 @@
-; https://leetcode.com/problems/valid-sudoku/submissions/1753662192
+; https://leetcode.com/problems/valid-sudoku/submissions/1754104093
+
 #lang racket
 (provide is-valid-sudoku)
 
 (define/contract (is-valid-sudoku board)
   (-> (listof (listof char?)) boolean?)
-    (valid? board 0 0 (list
-      (make-vector 90 0) ; rows
-      (make-vector 90 0) ; cols
-      (make-vector 90 0)))) ; areas
+    (valid? board 0 0 (counts
+      (make-vector 90 0)
+      (make-vector 90 0)
+      (make-vector 90 0))))
+
+(struct counts (rows cols areas))
 
 ; board without first cell
 (define (board-rest board)
@@ -27,9 +30,9 @@
 
 (define (checks-ok? num row col counts)
   (and
-    (seen? (+ (* 10 row) num) (first counts))
-    (seen? (+ (* 10 col) num) (second counts))
-    (seen? (+ (* 30 (quotient col 3)) (* 10 (quotient row 3)) num) (third counts))))
+    (seen? (+ (* 10 row) num) (counts-rows counts))
+    (seen? (+ (* 10 col) num) (counts-cols counts))
+    (seen? (+ (* 30 (quotient col 3)) (* 10 (quotient row 3)) num) (counts-areas counts))))
 
 (define (valid? board row col counts)
   (cond
@@ -39,3 +42,4 @@
       (if (or (not num) (checks-ok? num row col counts))
         (valid? (board-rest board) row (add1 col) counts)
         #f))]))
+
---
+++ 0/0036/6-rkt/test.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+racket solution.test.rkt
