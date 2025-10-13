2493ed5a61bfd84ee9336d6292ebe029aac04e99
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-10-13 18:06:30 +0200

    changes

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,10 +1,14 @@
-fn solve(arr: &[i32]) -> usize {
-    arr.len()
+struct Solution;
+
+impl Solution {
+    fn solve(arr: &[i32]) -> usize {
+        arr.len()
+    }
 }
 
 #[test]
 fn test_1() {
-    let actual = solve(&[1, 2, 3]);
+    let actual = Solution::solve(&[1, 2, 3]);
     let expected = 3;
     assert_eq!(actual, expected);
 }
