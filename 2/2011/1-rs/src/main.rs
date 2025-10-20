7717adfc4640cdfae82e4089bfc8c6b79d0f505b
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-10-20 08:50:40 +0200

    changes

---
+++ 2/2011/1-rs/src/main.rs
@@ -0,0 +1,36 @@
+struct Solution;
+
+impl Solution {
+    pub fn final_value_after_operations(operations: Vec<String>) -> i32 {
+        let mut result = 0;
+        for op in operations {
+            if op[1..2] == String::from("+") {
+                result += 1;
+            } else {
+                result -= 1;
+            }
+        }
+        result
+    }
+}
+
+#[test]
+fn test_1() {
+    let actual = Solution::final_value_after_operations(["--X","X++","X++"].map(String::from).to_vec());
+    let expected = 1;
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_2() {
+    let actual = Solution::final_value_after_operations(["++X","++X","X++"].map(String::from).to_vec());
+    let expected = 3;
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_3() {
+    let actual = Solution::final_value_after_operations(["X++","++X","--X","X--"].map(String::from).to_vec());
+    let expected = 0;
+    assert_eq!(actual, expected);
+}
