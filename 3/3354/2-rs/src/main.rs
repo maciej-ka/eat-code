33cc412d450cbed60079c93033ef557100254844
maciej-ka <maciej.kasprzyk.it@gmail.com>
2025-10-28 07:04:16 +0100

    changes

---
+++ 3/3354/2-rs/src/main.rs
@@ -0,0 +1,49 @@
+// https://leetcode.com/problems/make-array-elements-equal-to-zero/submissions/1813765469/
+
+struct Solution;
+
+impl Solution {
+    pub fn count_valid_selections(nums: Vec<i32>) -> i32 {
+
+        let sum = nums.iter().sum();
+        if sum <= 1 {
+            let len: i32 = nums.len().try_into().unwrap();
+            if sum == 0 { return len << 1}
+            if sum == 1 { return len - 1 }
+        }
+
+        let mut r: i32 = sum;
+        let mut l: i32 = 0;
+        let mut res = 0;
+
+        for val in nums {
+            if l > r + 1 { break }
+
+            if val == 0 {
+                let d = (r - l).abs();
+                if d == 0 { res += 2 }
+                if d == 1 { res += 1 }
+                continue;
+            }
+
+            l += val;
+            r -= val;
+        }
+
+        res
+    }
+}
+
+#[test]
+fn test_1() {
+    let actual = Solution::count_valid_selections(vec![1, 0, 2, 0, 3]);
+    let expected = 2;
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_2() {
+    let actual = Solution::count_valid_selections(vec![2, 3, 4, 0, 4, 1, 0]);
+    let expected = 0;
+    assert_eq!(actual, expected);
+}
