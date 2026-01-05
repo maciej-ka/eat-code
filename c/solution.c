1862958252f92517031c4408147bc3fc8f5091df
maciej-ka <maciej.kasprzyk.it@gmail.com>
2026-01-05 17:46:32 +0100

    changes

--- c/solution.c
+++ c/solution.c
@@ -1,27 +1,16 @@
 #include <stdio.h>
 #include <assert.h>
-#include <stdlib.h>
 
-int solve(int** nums, int size) {
+int solve(int* nums, int size) {
     return size;
 }
 
 void test1() {
-    int size = 3;
-    int** nums = malloc(sizeof(int*) * size);
-    for (int i = 0; i < size; i++) {
-        nums[i] = malloc(sizeof(int));
-        *nums[i] = i + 1;
-    }
-
+    int nums[] = {1, 2, 3};
+    int size = sizeof(nums) / sizeof(nums[0]);
     int result = solve(nums, size);
     assert(result == 3);
     printf("✓ test1 passed\n");
-
-    for (int i = 0; i < size; i++) {
-        free(nums[i]);
-    }
-    free(nums);
 }
 
 int main() {
