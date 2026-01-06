64befbe0cdfc70e3c495913e00b0a4883f4a4c5c
maciej-ka <maciej.kasprzyk.it@gmail.com>
2026-01-06 19:37:34 +0100

    changes

---
+++ cpp/solution.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+#include <vector>
+
+class Solution {
+public:
+    int solve(std::vector<int> nums) {
+        return nums.size();
+    }
+};
+
+TEST(SolutionTest, ReturnsCorrectLength) {
+    Solution solution;
+    std::vector<int> nums = {1, 2, 3};
+    EXPECT_EQ(solution.solve(nums), 3);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
