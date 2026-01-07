7436589865a76eced7e33eac34edb97e26f167fe
maciej-ka <maciej.kasprzyk.it@gmail.com>
2026-01-07 23:41:11 +0100

    changes

---
+++ erl/solution.erl
@@ -0,0 +1,10 @@
+-module(solution).
+-export([solve/1]).
+-include_lib("eunit/include/eunit.hrl").
+
+-spec solve(Nums :: [integer()]) -> integer().
+solve(Nums) ->
+    length(Nums).
+
+solve_1_test() ->
+    ?assertEqual(3, solve([1, 2, 3])).
