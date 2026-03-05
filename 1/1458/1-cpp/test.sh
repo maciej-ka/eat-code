c77b970ddb0dccafb7d59624044b39fe8c1983a7
maciej-ka <maciej.kasprzyk.it@gmail.com>
2026-03-05 09:33:41 +0100

    changes

--- 1/1458/1-cpp/test.sh
+++ 1/1458/1-cpp/test.sh
@@ -2,5 +2,7 @@
 g++ -std=c++17 solution.cpp \
     -I/opt/homebrew/include \
     -L/opt/homebrew/lib \
+    -g \
+    -fsanitize=address,undefined \
     -lgtest -lgtest_main -pthread \
     -o solution && ./solution
